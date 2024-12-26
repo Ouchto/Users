b788613741bace86cbcce9218651e31dd128e81e
Ouchto <mustapha.enaa@gmail.com>
2024-12-26 10:20:52 +0100

    Fonction pour ajouter un Utilisateur

--- src/Personne.java
+++ src/Personne.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Personne {
     protected String userNom;
     protected int userAge;
     protected String userCIN;
 
+    ArrayList<Utilisateur> users = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+
+    // --------------------------------------------- les constracteurs ---------------------------------------------
 
     public Personne() {}
 
@@ -12,6 +19,8 @@ public class Personne {
         this.userCIN = userCIN;
     }
 
+
+    // --------------------------------------------- Les getters and setters ---------------------------------------------
 
     public String getUserNom() {
         return userNom;
@@ -36,4 +45,51 @@ public class Personne {
     public void setUserCIN(String userCIN) {
         this.userCIN = userCIN;
     }
+
+
+    // --------------------------------------------- Les fonctions ---------------------------------------------
+
+    //fonction pour ajouter un Utilisateur
+    public void ajouterUtilisateur() {
+
+
+        System.out.print("Entrez le nom : ");
+        String nom = input.nextLine();
+
+        System.out.print("Entrez l'âge : ");
+        int age = input.nextInt();
+
+        System.out.print("Entrez le CIN : ");
+        String cin = input.nextLine();
+
+        System.out.print("Entrez l'email : ");
+        String email = input.nextLine();
+
+        System.out.print("Entrez le mot de passe : ");
+        String password = input.nextLine();
+
+        System.out.print("Entrez le rôle : ");
+        String roleName = input.nextLine();
+
+        Role role = new Role(roleName);
+        Utilisateur newUtilisateur = new Utilisateur(nom, age, cin, password, email, role);
+        users.add(newUtilisateur);
+
+        System.out.println("Utilisateur ajouté avec succès !");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
